e397850f6726be737b549847d60799691ab170cb
Rysalieva <aizirekrysalieva05@gmail.com>
2023-12-15 23:41:36 +0600

    second commit

--- dz02_Aizirek_33-03/app.js
+++ dz02_Aizirek_33-03/app.js
@@ -1,70 +1,60 @@
-// Конструктор StarbucksCoffeeCompany
-function StarbucksCoffeeCompany(location, menu, employees) {
-    this.location = location;
-    this.menu = menu;
-    this.employees = employees;
+// 1.Starbucks Coffee Company
+var starbucks = {
+    name: "Starbucks Coffee Company",
+    foundedYear: 1971,
+    locations: ["1st Avenue, Seattle", "Times Square, New York"],
+    popularDrink: "Caramel Macchiato",
+    isPubliclyTraded: true,
+    revenue: "$26.7 billion (2021)"
+};
 
-    // Метод для отображения информации о компании
-    this.displayInfo = function () {
-        console.log(`Starbucks Coffee Company at ${this.location}`);
-        console.log("Menu:");
-        this.menu.forEach(item => console.log(`- ${item}`));
-        console.log(`Number of employees: ${this.employees.length}`);
-    };
-}
+// о компании в консоль
+console.log(`Welcome to ${starbucks.name}! Founded in ${starbucks.foundedYear}.`);
+console.log(`We have locations in ${starbucks.locations.length} cities.`);
+console.log(`Try our popular drink: ${starbucks.popularDrink}.`);
+console.log(`As of 2021, our revenue is ${starbucks.revenue}.`);
 
-// Пример использования
-const starbucksLocation = "123 Main Street";
-const starbucksMenu = ["Latte", "Cappuccino", "Mocha", "Espresso", "Frappuccino"];
-const starbucksEmployees = ["Barista1", "Barista2", "Cashier1"];
-
-const starbucks = new StarbucksCoffeeCompany(starbucksLocation, starbucksMenu, starbucksEmployees);
-starbucks.displayInfo();
-
-for (let number = 0; number <= 100; number++) {
-    if (number % 3 === 0 && number % 5 === 0) {
+// 2. "FizzBuzz"
+for (var i = 0; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
-    } else if (number % 3 === 0) {
+    } else if (i % 3 === 0) {
         console.log("Fizz");
-    } else if (number % 5 === 0) {
+    } else if (i % 5 === 0) {
         console.log("Buzz");
     } else {
-        console.log(number);
+        console.log(i);
     }
 }
 
-function getSeason(month) {
-    let season = null;
+// 3.для определения сезона года
+var month = prompt("Введите месяц (от 1 до 12):");
 
-    switch (month) {
-        case 1:
-        case 2:
-        case 12:
-            season = "Winter";
-            break;
-        case 3:
-        case 4:
-        case 5:
-            season = "Spring";
-            break;
-        case 6:
-        case 7:
-        case 8:
-            season = "Summer";
-            break;
-        case 9:
-        case 10:
-        case 11:
-            season = "Autumn";
-            break;
-        default:
-            break;
-    }
-
-    console.log(`Season: ${season}`);
+switch (parseInt(month)) {
+    case 12:
+    case 1:
+    case 2:
+        console.log("Зима");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log("Весна");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        console.log("Лето");
+        break;
+    case 9:
+    case 10:
+    case 11:
+        console.log("Осень");
+        break;
+    default:
+        console.log("Некорректный ввод месяца.");
 }
 
-getSeason(3);
-
-const myArray = [[[[[[[[[[[2]]]]]]]]]]];
-console.log(myArray[0][0][0][0][0][0][0][0][0][0][0]);
+// 4. вывод цифры 2 из массива
+var nestedArray = [[[[[[[[[[[2]]]]]]]]]]];
+console.log(nestedArray[0][0][0][0][0][0][0][0][0][0][0]);
